313fdc715a3282b8f56880e0858ac3b43a156e2c
Chris Sharp <csharp@georgialibraries.org>
2011-03-02 04:59:45 -0800

    adding modifications copyright info

--- eg_installer.sh
+++ eg_installer.sh
@@ -2,7 +2,9 @@
 # -----------------------------------------------------------------------
 # Copyright (C) 2009  Equinox Software Inc.
 # Bill Erickson <erickson@esilibrary.com>
-# 
+# Modifications (c) 2011 Georgia Public Library Service 
+# Chris Sharp <csharp@georgialibraries.org>
+#
 # This program is free software; you can redistribute it and/or
 # modify it under the terms of the GNU General Public License
 # as published by the Free Software Foundation; either version 2
